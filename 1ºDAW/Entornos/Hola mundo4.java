c0efd8d06c963fc9932b5a6b0387564accdf07de
Daviiid22 <150366915+Daviiid22@users.noreply.github.com>
2023-11-09 17:33:39 +0100

    09/11/2023

---
+++ 1ºDAW/Entornos/Hola mundo4.java
@@ -0,0 +1,7 @@
+class HolaMundo4 {
+		
+		public static void main(String[] args) {
+			
+			System.out.print("Buenas soy David");
+			}
+}
---
+++ 1ºDAW/Programacion/Ejercicios/Ejercicio if else.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumnat
+ */
+public class Ejercicios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner sc = new Scanner(System.in);
+    float numero;
+    System.out.println("Introduce un numero: ");
+    numero = sc. nextFloat();
+    
+    if(numero > -1 && numero < 1 && numero !=0){
+        System.out.println("Correcto");
+    }
+    else{
+        System.out.println("ERROR");
+    }
+    
+    } 
+}
